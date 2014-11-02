e0e3e7e70a05bab28c4b1a658e66c3a2cc7843fc
sarah alsayyahi <alsayahi10@gmail.com>
2014-11-02 01:02:33 -0600

    hi

--- app/controllers/strategies_controller.rb
+++ app/controllers/strategies_controller.rb
@@ -1,27 +1,35 @@
 class StrategiesController < ApplicationController
 
 
-	
+
 	def new
 
 	end
     
     def show
         
+         @strategy = Strategy.find(params[:id])
+         @strategies = Strategy.all
     end
 
 	def create
         
-        @strategy = Strategy.new(params[:strategy])
+        @strategy = Strategy.new(strategy_params)
         
         @strategy.save
         redirect_to @strategy
+    end
+    
+    private
+    def strategy_params
+        params.require(:strategy).permit(:id, :title, :text, :body, :tech, :source)
         
 	end
     
 
 	def update
-
+        
+        
 	end
 
 	def destroy
