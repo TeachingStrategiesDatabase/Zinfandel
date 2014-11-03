ce67b6b9eb66c444e846735cd3d4154c09729a03
WallyWest121 <mayedamitch@gmail.com>
2014-11-02 20:30:35 -0700

    added routes changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'new' => 'strategies#new'
+  get 'home' => 'example#home'
+  post 'create' => 'strategies#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
