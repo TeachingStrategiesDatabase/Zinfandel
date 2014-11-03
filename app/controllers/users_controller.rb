fb3bb50dcfb68569e7c3e95df86ddc8e02c31c88
slswee <sally.xinli@gmail.com>
2014-11-02 21:16:28 -0700

    sally

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,29 @@
 class UsersController < ApplicationController
 
 	def new
-
 	end
 
 	def create
 # check to see that confirm_password == password before continuing
-	  if confirm_password != password
-		@errors = @users.errors
-#		redirect_to (/"create")
+	  if[:confirm_password] != [:password]
+#		@errors = @users.errors
+		redirect_to ("/new")
 	  else
 
 		@user = User.new(user_info_params)
 		@user.save
-	 end
-
-  private
-  def user_info_params
-    params[:user][:name] = params[:user][:first_name] + ' ' + params[:user][:last_name]
-    params.require(:user).permit(:name, :email, :password)
-  end
+	  end
+	end
+	 private
+   	   def user_info_params
+	    params[:user][:name] = params[:user][:first_name] + ' ' + params[:user][:last_name]
+   	   params.require(:user).permit(:name, :email, :password)
+ 	 end
 
 	def update
-
 	end
 
 	def destroy
-
 	end
+
 end
