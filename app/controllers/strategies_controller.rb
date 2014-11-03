0a867ae02a87db0a1d6dfba152b9b2f853dd10a5
sarah alsayyahi <alsayahi10@gmail.com>
2014-11-02 19:20:54 -0700

    hi

--- app/controllers/strategies_controller.rb
+++ app/controllers/strategies_controller.rb
@@ -1,32 +1,37 @@
 class StrategiesController < ApplicationController
 
-
+   
 
 	def new
-
+        @strategy = Strategy.new
 	end
     
+    def index
+        
+    end
     
 
 	def create
-        
-    
+      render plain: params[:strategy].inspect
     end
         
         
     def show
+         @strategies=Strategy.all   
             
-            @strategy= Strategy.find(params[:id])
             
     end
         
 	def update
-        @strategies=Strategy.all
         
+        @strategy= Strategy.find(params[:id])
 	end
 
 	def destroy
-
+        
+        @strategy = Strategy.find(params[:id])
+        @strategy.destroy
+        redirect_to :action => 'show'
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  # default_url_options :host => "localhost:3000"
+    get 'strategies/show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   resources :strategies
   # You can have the root of your site routed with "root"
-  root 'strategies#update'
+  root 'strategies#show'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
