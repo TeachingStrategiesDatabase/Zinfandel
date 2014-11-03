de3e69184b4c886a29a8a00317269e02efe049ed
WallyWest121 <mayedamitch@gmail.com>
2014-11-02 19:17:29 -0700

    First commit

--- app/controllers/strategies_controller.rb
+++ app/controllers/strategies_controller.rb
@@ -5,11 +5,21 @@ class StrategiesController < ApplicationController
 	end
 	
 	def new
-
+		@strategy = Strategy.new
 	end
 
 	def create
-
+		@strategy = Strategy.new(strategy_params)
+		@strategy.created_at = Time.new
+		@strategy.updated_at = Time.new
+ 
+  		if @strategy.save
+  			redirect_to ("/home")
+  		else 
+  			render ("new")
+  		end
+		#render plain: params[:strategy].inspect
+		#redirect_to("/home")
 	end
 
 	def update
@@ -19,5 +29,11 @@ class StrategiesController < ApplicationController
 	def destroy
 
 	end
+
+	private
+		def strategy_params
+			#params.require(:title,:department,:subject,:body).permit(:tech)
+			params.require(:strategy).permit(:title,:body,:tech)
+		end
 
 end
